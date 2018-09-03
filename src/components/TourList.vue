e8da258ee1170b35afb732a1e023a9b49dc8cd80
derrabauke <pfralf@googlemail.com>
2018-09-03 18:41:51 +0200

    Added fetching feedback in UI for table contents

--- src/components/TourList.vue
+++ src/components/TourList.vue
@@ -1,23 +1,33 @@
 <template>
-    <div>
-        <table>
-            <thead>
-                <th rowspan="2" width="60vw">Weg</th>
-                <th width="40vw">Grad</th>
-            </thead>
-            <tr class="listItem" v-for="tour in allTours" :key="tour.Name">
-                <template v-if="getCellColor()">
-                    <td class="colored">{{ tour.Weg }}<br/>
-                    <span class="tour-details">{{ tour.Gipfel }} ( {{ tour.Gebiet }} )</span></td>
-                    <td class="grad-style colored">{{tour.af}}</td>
-                </template>
-                <template v-else>
-                    <td>{{ tour.Weg }}<br/>
-                    <span class="tour-details">{{ tour.Gipfel }} ( {{ tour.Gebiet }} )</span></td>
-                    <td class="grad-style">{{tour.af}}</td>
-                </template>
-            </tr>
-        </table>
+    <div class="table">
+        <div class="loading" v-if="loading">
+            Loading...
+        </div>
+
+        <div v-if="error" class="error">
+            An error occured: {{ error }}
+        </div>
+
+        <div v-if="allTours" class="content">
+            <table>
+                <thead>
+                    <th rowspan="2" width="60vw">Weg</th>
+                    <th width="40vw">Grad</th>
+                </thead>
+                <tr class="listItem" v-for="tour in allTours" :key="tour.Name">
+                    <template v-if="getCellColor()">
+                        <td class="colored">{{ tour.Weg }}<br/>
+                        <span class="tour-details">{{ tour.Gipfel }} ( {{ tour.Gebiet }} )</span></td>
+                        <td class="grad-style colored">{{tour.af}}</td>
+                    </template>
+                    <template v-else>
+                        <td>{{ tour.Weg }}<br/>
+                        <span class="tour-details">{{ tour.Gipfel }} ( {{ tour.Gebiet }} )</span></td>
+                        <td class="grad-style">{{tour.af}}</td>
+                    </template>
+                </tr>
+            </table>
+        </div>
     </div>
 </template>
 
@@ -27,8 +37,9 @@ export default {
     data () {
         return {
             cellColor: true,
-            isFetched: false,
-            allTours: []
+            loading: false,
+            error: null,
+            allTours: null
         }
     },
     created () {
@@ -39,16 +50,29 @@ export default {
             return this.cellColor = !this.cellColor;
         },
         fetchTourData() {
+            function status(response) {
+                if (response.status >= 200 && response.status < 300) {
+                    return Promise.resolve(response)
+                } else {
+                    return Promise.reject(new Error(response.statusText))
+                }
+            }
+            this.loading = true
             if ( !this.isFetched ){
                 let con = console;
                 fetch('https://api.myjson.com/bins/nbj3g')
+                .then( status )
                 .then( response => response.json())
                 .then( json => {
                     this.allTours = json
-                    this.isFetched = true
+                    this.loading = false
                     if ( process.env.NODE_ENV !== 'production') {
                         con.log(json.length + "tours where fetched")
                     }
+                })
+                .catch( error => {
+                    this.error = error
+                    this.loading = false
                 })
             }
         }
